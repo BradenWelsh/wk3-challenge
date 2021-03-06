698add2e02fdc7ce3db82ea3210db83013723a38
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-03-06 17:19:49 -0600

    Added window alerts and vars

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,17 +1,28 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var lengthChar = window.prompt("How long do you want the password to be? 8-128");
+  if(lengthChar <=7 || lengthChar >=129){
+    window.alert ("You didn't put a valid number, please try again!")
+    return;
+  }
+
+  var numChar = window.confirm("Do you want numbers?");
+  var upChar = window.confirm("Do you want uppercase letters?");
+  var lowChar = window.confirm("Do you want lowercase letters?");
+
+  var numberChars = "0123456789";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var allChars = numberChars + upperChars + lowerChars;
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
